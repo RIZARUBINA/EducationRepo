e59b476bb47ae3ce5369915ca7a771f31620135a
Margarita Zarubina <margarita.zarubina@mera.com>
2019-09-04 14:35:49 +0300

    Empty project creation

---
+++ EducationRepo/EducationRepo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EducationRepo
+//
+//  Created by Margarita Zarubina on 04/09/2019.
+//  Copyright © 2019 Margarita Zarubina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
